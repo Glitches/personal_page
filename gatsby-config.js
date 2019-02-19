dba11f17d3f6c2ad48fdee12a2e73ac1bf0d7b84
Andrea Ceccarelli <gltch2003@gmail.com>
2019-02-19 22:52:44 +0100

    graphql experiment

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,24 @@
 module.exports = {
   plugins: [
+    {
+      resolve: 'gatsby-source-graphql',
+      options: {
+        // This type will contain remote schema Query type
+        typeName: 'SWAPI',
+        // This is field under which it's accessible
+        fieldName: 'swapi',
+        // Url to query from
+        url: 'https://api.graphcms.com/simple/v1/swapi',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-material-ui',
+      options: {
+        theme: {
+          primaryColor: '#9c27b0',
+        },
+      },
+    },
     'gatsby-plugin-sass',
     {
       resolve: 'gatsby-plugin-postcss',
