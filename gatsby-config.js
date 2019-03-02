997e63b24f4b2bd8ae57baf408483c33ade24362
Andrea Ceccarelli <gltch2003@gmail.com>
2019-03-02 23:09:24 +0100

    latest updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,8 @@ module.exports = {
     {
       resolve: 'gatsby-source-graphql',
       options: {
-        // This type will contain remote schema Query type
         typeName: 'SWAPI',
-        // This is field under which it's accessible
         fieldName: 'swapi',
-        // Url to query from
         url: 'https://api.graphcms.com/simple/v1/swapi',
       },
     },
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { graphql } from 'gatsby';
+import * as React from 'react';
+import { graphql, StaticQuery } from 'gatsby';
 import getTime from 'date-fns/get_time';
 import { Flex } from 'reflexy';
 import { Button } from '../lib/components/Button/index';
@@ -17,8 +17,27 @@ export default (): JSX.Element => (
         <span>HEADER</span>
       </Header>
       <Flex className="base">
-        <Button color="red" label="house" onClick={() => console.log('ciao')} />
-        <div>{timestamp}</div>
+        {/* <Button color="red" label="house" onClick={() => console.log('ciao')} /> */}
+        <StaticQuery
+          query={graphql`
+            query {
+              swapi {
+                Starship(name: "Millennium Falcon") {
+                  name
+                  hyperdriveRating
+                  pilots(orderBy: height_DESC) {
+                    name
+                    height
+                    homeworld {
+                      name
+                    }
+                  }
+                }
+              }
+            }
+          `}
+          render={data => console.log(data)}
+        />
       </Flex>
     </Layout>
   </React.Fragment>
