81ac70f95ec45622c8bd461b465c71cfa6440c5c
Andrea <andrea.ceccarelli@facile.it>
2019-03-07 22:15:00 +0100

    small updaet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import getTime from "date-fns/get_time";
 import { Flex } from "reflexy";
-import { Button } from "../lib/components/Button/index";
 
 import "./index.scss";
 import { Header } from "../lib/components/Header";
 import { Layout } from "../lib/components/Layout/Layout";
-
-const timestamp: number = getTime(new Date());
 
 class app extends React.PureComponent {
 	render() {
@@ -15,12 +12,27 @@ class app extends React.PureComponent {
 			<div className="base">
 				<Layout maxWidth={85}>
 					<Header display="sticky">
-						<span>HEADER</span>
+						<Flex justifyContent="center">
+							<h1>Andrea Ceccarelli</h1>
+						</Flex>
 					</Header>
-					<Flex>
-						<Button color="red" label="house" onClick={() => console.log("ciao")} />
-						<div>{timestamp}</div>
-					</Flex>
+					<div className="body-content">
+						<Flex>
+							<code>
+								Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+								laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
+								architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
+								sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+								voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit
+								amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut
+								labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
+								nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+								consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
+								nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
+								pariatur?
+							</code>
+						</Flex>
+					</div>
 				</Layout>
 			</div>
 		);
