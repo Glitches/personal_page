978caa77376a966fe42750b5d24c4f33ceb914fc
Andrea <andrea.ceccarelli@facile.it>
2020-02-03 23:07:08 +0100

    add ref still not working

--- src/lib/components/Tabs/Tabs.tsx
+++ src/lib/components/Tabs/Tabs.tsx
@@ -12,15 +12,30 @@ export interface Props {
 }
 
 export const Tabs: React.FC<Props> = ({ tabs }) => {
+    const [selectedState, setSelected] = React.useState('home')
+    const selectedRef = React.useRef<HTMLDivElement>(null)
+    const width = selectedRef.current ? selectedRef.current.offsetWidth : '1rem'
+
+    console.log(selectedRef.current ? selectedRef.current.getClientRects : null)
     return (
-        <div className='tabs'>
-            {
-                tabs.map(tab => (
-                    <div className={`tab-element ${tab.selected ? 'selected' : undefined}`}>
-                        {tab.label}
-                    </div>
-                ))
-            }
+        <div className="tabs-line">
+            <div className='tabs'>
+                {
+                    tabs.map((tab: TabType, index: number) => (
+                        <>
+                            <div
+                                className={`tab-element ${selectedState === tab.label ? 'selected' : null}`}
+                                key={index + tab.label}
+                                ref={selectedState === tab.label ? selectedRef : null}
+                                onClick={() => setSelected(tab.label)}
+                            >
+                                {tab.label}
+                            </div>
+                        </>
+                    ))
+                }
+            </div>
+            <div className="tab-element-selected" style={{ width: width }} />
         </div>
     )
 }
